fd38cfe414e371ce80a0d6f51e4436424e6033d5
arseny <43606985+ArsenyOK@users.noreply.github.com>
2022-05-09 18:45:01 +0300

    Added prepared statements query

--- select.php
+++ select.php
@@ -1,22 +1,37 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "Film");
- 
-if($connection === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+
+$dbh = new PDO('mysql:host=localhost;dbname=Film', 'root', '');
+
+
+$name = $_POST["genre"];
+
+$sth = $dbh->prepare('SELECT f.name from film f, film_genre, genre g
+where ID_Film=FID_Film and FID_Genre=ID_Genre and g.title=?');
+$sth->execute(array($name));
+$res = $sth->fetchAll();
+
+foreach ($res as $row) {
+    print_r($row[0]);
 }
+
+// $connection = mysqli_connect("localhost", "root", "", "Film");
  
-$name = mysqli_real_escape_string($connection, $_REQUEST['name']);
+// if($connection === false){
+//     die("ERROR: Could not connect. " . mysqli_connect_error());
+// }
+ 
+// $name = mysqli_real_escape_string($connection, $_REQUEST['name']);
 
-$sql = 'SELECT f.name from film f, film_genre, genre g
-    where ID_Film=FID_Film and FID_Genre=ID_Genre and g.title="Ужасы"';
+// $sql = 'SELECT f.name from film f, film_genre, genre g
+//     where ID_Film=FID_Film and FID_Genre=ID_Genre and g.title="Ужасы"';
 
-$result = mysqli_query($connection, $sql);
+// $result = mysqli_query($connection, $sql);
 
-    while($row = $result->fetch_assoc())
-    {
-        echo  $row['name'];
+//     while($row = $result->fetch_assoc())
+//     {
+//         echo  $row['name'];
 
-    }
+//     }
 
-mysqli_close($link);
+// mysqli_close($link);
 ?>
